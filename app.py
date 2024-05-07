c0f9a0656cdee186406b06f84716737e60c47c85
DevUserFromBeginning <169102380+DevUserFromBeginning@users.noreply.github.com>
2024-05-07 20:33:19 +0000

    Adding dinamyc data

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+lst_jobs = [
+    {
+        'id' : 1,
+        'title' : "Data Analyst",
+        'location' : "Bengaluru, India",
+        'salary' : "Rs. 10,00,000"
+    },
+    {
+        'id' : 2,
+        'title' : "Data Scientist",
+        'location' : "Delhi, India",
+        'salary' : "Rs. 20,00,000"
+    },
+    {
+        'id' : 3,
+        'title' : "Frontend Engineer",
+        'location' : "Remote"
+    },
+    {
+        'id' : 4,
+        'title' : "Backend Engineer",
+        'location' : "San Francisco, USA",
+        'salary' : "US$, 120.000"
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=lst_jobs, company_name='Jovian INC')
 
 
 if __name__ == "__main__":
