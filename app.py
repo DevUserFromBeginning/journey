82133e6ec634d2d8f068223af8289831326f14e6
DevUserFromBeginning <169102380+DevUserFromBeginning@users.noreply.github.com>
2024-05-07 00:27:39 +0000

    Added HTML and CSS 1st stage (no bootstrap)

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Ernesto!</p>"
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
   # remember to change debug to False in production
